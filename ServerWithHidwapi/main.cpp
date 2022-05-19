fbb93b8931e07eb6b9b941eb0fc01575c3752efe
Ren-eee <milostanov@mail.ru>
2022-05-19 18:51:56 +0300

    cleaning 2

--- ServerWithHidwapi/main.cpp
+++ ServerWithHidwapi/main.cpp
@@ -23,11 +23,7 @@ struct modbus{
     char data[MAXBYTE-8] = "";
     };
 
-union union_type
-{
-unsigned char elem16[2];
-uint16_t elem10;
-};
+
 
 union union_type{
     unsigned char elem16[2];
@@ -48,71 +44,9 @@ void MakeMinimumBrightness(hid_device *handle, unsigned char *buf , modbus* pack
 void PaintOverTheScreen(hid_device *handle, unsigned char *buf , modbus* package , char * mas);
 void MeasureVoltageAndChangeRGB(hid_device *handle, unsigned char *buf, union_type * diod_color, int res, modbus* package , char * mas);
 
-int all_hid_info(){
+int all_hid_info();
 
-	struct hid_device_info *devs, *cur_dev;
-
-	printf("hidapi test/example tool. Compiled with hidapi version %s, runtime version %s.\n", HID_API_VERSION_STR, hid_version_str());
-	if (hid_version()->major == HID_API_VERSION_MAJOR && hid_version()->minor == HID_API_VERSION_MINOR && hid_version()->patch == HID_API_VERSION_PATCH) {
-		printf("Compile-time version matches runtime version of hidapi.\n\n");
-	}
-	else {
-		printf("Compile-time version is different than runtime version of hidapi.\n]n");
-	}
-
-	if (hid_init())
-		return -1;
-
-    // находим все устройства USB HID, печатаем содержимое дескриптора устройства, доступное через драйвер
-    devs = hid_enumerate(0x0, 0x0);
-	cur_dev = devs;
-	while (cur_dev) {
-		printf("Device Found\n  type: %04hx %04hx\n  path: %s\n  serial_number: %ls", cur_dev->vendor_id, cur_dev->product_id, cur_dev->path, cur_dev->serial_number);
-		printf("\n");
-		printf("  Manufacturer: %ls\n", cur_dev->manufacturer_string);
-		printf("  Product:      %ls\n", cur_dev->product_string);
-		printf("  Release:      %hx\n", cur_dev->release_number);
-		printf("  Interface:    %d\n",  cur_dev->interface_number);
-		printf("  Usage (page): 0x%hx (0x%hx)\n", cur_dev->usage, cur_dev->usage_page);
-		printf("\n");
-		cur_dev = cur_dev->next;
-	}
-	hid_free_enumeration(devs);
-
-}
-
-int open_hid_info(hid_device *handle, int* res){
-    wchar_t wstr[MAX_STR];
-	// Read the Manufacturer String
-	wstr[0] = 0x0000;
-	*res = hid_get_manufacturer_string(handle, wstr, MAX_STR);
-	if (*res < 0)
-		printf("Unable to read manufacturer string\n");
-	printf("Manufacturer String: %ls\n", wstr);
-
-	// Read the Product String
-	wstr[0] = 0x0000;
-	*res = hid_get_product_string(handle, wstr, MAX_STR);
-	if (*res < 0)
-		printf("Unable to read product string\n");
-	printf("Product String: %ls\n", wstr);
-
-	// Read the Serial Number String
-	wstr[0] = 0x0000;
-	*res = hid_get_serial_number_string(handle, wstr, MAX_STR);
-	if (res < 0)
-		printf("Unable to read serial number string\n");
-	printf("Serial Number String: (%d) %ls", wstr[0], wstr);
-	printf("\n");
-
-	// Read Indexed String 1
-	wstr[0] = 0x0000;
-	*res = hid_get_indexed_string(handle, 1, wstr, MAX_STR);
-	if (*res < 0)
-		printf("Unable to read indexed string 1\n");
-	printf("Indexed String 1: %ls\n", wstr);
-	return 0;
-}
+int open_hid_info(hid_device *handle, int* res);
 
 
 int main(int argc, char* argv[])
@@ -218,7 +152,7 @@ int main(int argc, char* argv[])
 }
 
 
-void MakeMaximumBrightness(hid_device *handle, unsigned char *buf){
+
 
 void MakeMaximumBrightness(hid_device *handle, unsigned char *buf , modbus* package , char * mas)
 {
@@ -232,7 +166,7 @@ void MakeMaximumBrightness(hid_device *handle, unsigned char *buf , modbus* pack
 }
 
 
-void MakeMinimumBrightness(hid_device *handle, unsigned char *buf){
+
 
 void MakeMinimumBrightness(hid_device *handle, unsigned char *buf , modbus* package , char * mas)
 {
@@ -246,7 +180,6 @@ void MakeMinimumBrightness(hid_device *handle, unsigned char *buf , modbus* pack
 }
 
 
-void PaintOverTheScreen(hid_device *handle, unsigned char *buf){
 
 void PaintOverTheScreen(hid_device *handle, unsigned char *buf , modbus* package , char * mas)
 	{
@@ -266,14 +199,10 @@ void PaintOverTheScreen(hid_device *handle, unsigned char *buf , modbus* package
 
 		strcpy(package->data, "Экран устройства закрашен");
 
-		//memcpy(bytes, &package, sizeof(modbus));//кодируем
+		memcpy(bytes, &package, sizeof(modbus));//кодируем
 }
 
-void MeasureVoltageAndChangeRGB(hid_device *handle, unsigned char *buf, union_type * diod_color, int res, modbus* package, char bytes[]){
 
-
-		memcpy(mas, package, sizeof(modbus));//кодируем
-	}
 
 void MeasureVoltageAndChangeRGB(hid_device *handle, unsigned char *buf, union_type * diod_color, int res, modbus* package, char * mas)
 {
